a52e0806a74b3f8593bec8a7c158a21ce161d9be
Jenny Huang <jennyyyhuang@yahoo.de>
2025-03-26 02:53:31 +0100

    updated design and text

--- src/TracklistGenerator.js
+++ src/TracklistGenerator.js
@@ -105,18 +105,26 @@ const TracklistGenerator = () => {
   };
 
   return (
-    <div className="flex flex-col min-h-screen bg-[#121212] text-[#e0e0e0] p-8 font-mono">
+    <div className="flex flex-col min-h-screen bg-color_background text-color_text px-10 py-8 font-mono">
       <div className="flex-grow">
-        <h1 className="text-3xl font-bold mb-8 text-center">TRACKLIST GENERATOR</h1>
-        <div className="mt-16 text-center"></div>
+        {/* <h1 className="text-3xl font-bold mb-8 text-center">TRACKLIST GENERATOR</h1> */}
+        <div className="flex justify-center">
+          <img
+            src="/Logo_smaller.png"
+            className="w-2/3 sm:w-1/3 lg:w-1/5"
+            alt="Tracklist Generator"
+          />
+        </div>
+
+        {/* <div className="mt-16 text-center"></div> */}
         <div
-          className={`mb-8 border-2 border-dashed border-lime-200 rounded-lg p-0 text-center cursor-pointer transition-all duration-300 ease-in-out ${dragActive ? 'border-gray-400 bg-gray-800' : ''}`}
+          className={`my-12 p-8 border-2 border-dashed border-color_text rounded-lg p-0 text-center cursor-pointer transition-all duration-300 ease-in-out bg-color_text ${dragActive ? 'border-gray-400 bg-opacity-15' : 'hover:bg-opacity-25 bg-opacity-10'}`}
           onDragEnter={handleDrag}
           onDragLeave={handleDrag}
           onDragOver={handleDrag}
           onDrop={handleDrop}>
           <label className="cursor-pointer w-full h-full block">
-            <div className="text-lg text-lime-200 p-8">
+            <div className="text-lg p-8 font-bold">
               DROP FILE HERE OR CLICK TO SELECT (.TXT FORMAT ONLY)
             </div>
             <input type="file" accept=".txt" onChange={handleFileUpload} className="hidden" />
@@ -124,19 +132,20 @@ const TracklistGenerator = () => {
         </div>
         {tracklist.length === 0 && (
           <div className="mt-16 text-center">
-            <p className="text-lg">
-              This Tracklist Generator helps you generate a tracklist from your exported Rekordbox
-              playlist.
+            <p className="">
+              This Tracklist Generator helps you convert your Rekordbox playlist to a Tracklist
+              format.
             </p>
-            <p className="text-lg mt-8">To export a playlist:</p>
-            <ol className="list-decimal list-inside text-sm mt-4 space-y-2">
-              <li>Open Rekordbox and select your playlist (e.g. from the history).</li>
+            <p className="mt-8">How-to:</p>
+            <ol className="list-decimal list-inside space-y-2 mt-2">
+              <li>Select your playlist in Rekordbox (e.g. from the history)</li>
               <li>
-                Right-click and choose <strong>Export Playlist</strong>.
+                Right-click and choose <strong>Export Playlist</strong>
               </li>
               <li>
-                Select <strong>Export as .txt file</strong>.
+                Select <strong>Export as .txt file</strong>
               </li>
+              <li>Drop the .txt file in the area above</li>
             </ol>
           </div>
         )}
@@ -148,23 +157,19 @@ const TracklistGenerator = () => {
               <div className="relative table-container overflow-x-auto">
                 <table className="w-full border-collapse ">
                   <thead>
-                    <tr className="bg-[#1a1a1a]">
+                    <tr className="bg-color_text bg-opacity-90 text-color_background ">
                       {columns.map((column, index) => (
-                        <th
-                          key={index}
-                          className="p-3 text-left text-xs uppercase tracking-wider border-b border-gray-800">
+                        <th key={index} className="p-3 text-left text-xs uppercase tracking-wider ">
                           {column}
                         </th>
                       ))}
                     </tr>
                   </thead>
-                  <tbody className="divide-y divide-gray-800">
+                  <tbody className="divide-y divide-color_text">
                     {tracklist
                       .slice(0, isExpanded ? tracklist.length : collapsedRows)
                       .map((track, index) => (
-                        <tr
-                          key={index}
-                          className="hover:bg-[#1a1a1a] transition-colors duration-150 ease-in-out">
+                        <tr key={index} className="transition-colors duration-150 ease-in-out">
                           {columns.map((column, colIndex) => (
                             <td key={colIndex} className="p-3 text-sm whitespace-nowrap">
                               {track[column]}
@@ -180,13 +185,13 @@ const TracklistGenerator = () => {
                   <div
                     onClick={toggleRows}
                     className="w-full cursor-pointer text-center py-4 flex justify-center">
-                    <FaChevronUp className="text-lime-200 text-2xl" />
+                    <FaChevronUp className="text-color_text text-2xl" />
                   </div>
                 ) : (
                   <div
                     onClick={toggleRows}
-                    className="absolute bottom-0 w-full bg-gradient-to-t from-[#121212] via-[#121212]/90 to-transparent cursor-pointer text-center py-4 flex justify-center">
-                    <FaChevronDown className="text-lime-200 text-2xl" />
+                    className="absolute bottom-0 w-full bg-gradient-to-t from-color_text via-color_text/30 to-transparent cursor-pointer text-center py-4 flex justify-center">
+                    <FaChevronDown className="text-color_text text-2xl" />
                   </div>
                 ))}
             </div>
@@ -200,22 +205,22 @@ const TracklistGenerator = () => {
                   onChange={() => setShowNumbers(!showNumbers)}
                   className="sr-only peer"
                 />
-                <div className="relative w-11 h-6 bg-gray-200 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-lime-300 dark:peer-focus:ring-lime-800 rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:start-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-lime-600"></div>
+                <div className="relative w-11 h-6 bg-gray-200 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-text_color dark:peer-focus:ring-text_color rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:start-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-color_text"></div>
                 <span className="ms-3 text-lg">Show Track Numbers</span>
               </label>
-              <pre className="bg-[#1a1a1a] p-4 rounded-lg whitespace-pre-wrap">
+              <pre className="bg-color_text bg-opacity-90 text-color_background p-4 rounded-lg whitespace-pre-wrap">
                 {formattedTracklist}
               </pre>
               <div className="flex flex-row mt-4 items-center space-x-4">
                 <button
                   onClick={copyToClipboard}
-                  className="bg-gray-800 hover:bg-gray-700 text-white font-bold py-2 px-4 rounded transition-colors duration-150 ease-in-out">
+                  className="bg-color_text bg-opacity-90 hover:bg-opacity-100 text-color_background py-2 px-4 rounded transition-colors duration-150 ease-in-out">
                   COPY TO CLIPBOARD
                 </button>
                 <div>
                   {copyFeedback && (
                     <FaCheck
-                      className="text-lime-400"
+                      className="text-text_color"
                       style={{ animation: 'fadeInOut 1.5s ease-in-out' }}
                     />
                   )}
@@ -225,12 +230,12 @@ const TracklistGenerator = () => {
           </>
         )}
       </div>
-      <footer className="mt-12 text-end text-xs text-gray-400">
+      <footer className="mt-12 text-end text-xs">
         <p>
           Created by{' '}
           <a
             href="https://jennyhuang.de"
-            className="text-lime-200 underline"
+            className="underline"
             target="_blank"
             rel="noopener noreferrer">
             Jenny Huang
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,19 @@
 module.exports = {
   content: ['./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
-    extend: {}
+    extend: {
+      fontFamily: {
+        mono: ['Fira Code', 'monospace']
+      },
+      colors: {
+        color_background: {
+          DEFAULT: '#F4E2D4'
+        },
+        color_text: {
+          DEFAULT: '#DA687F'
+        }
+      }
+    }
   },
   plugins: []
 };
